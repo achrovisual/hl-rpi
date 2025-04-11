f49ccce1a8b32bf805ceabffa46ddac2257b2410
Eugenio Pastoral <pastoraleugenio@gmail.com>
2025-04-11 20:22:00 +0800

    Add UpSnap commands

---
+++ services/smart_home/config/home-assistant/commands/pc_on.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -s "$UPSNAP_URL/api/upsnap/wake/$UPSNAP_DEVICE_ID" \
+    -H "Authorization: Bearer $(curl -s -X POST "$UPSNAP_URL/api/collections/_superusers/auth-with-password" \
+        -H "Content-Type: application/json" \
+        -d "{\"identity\": \"$UPSNAP_USERNAME\", \"password\": \"$UPSNAP_PASSWORD\"}" | jq -r '.token')"
